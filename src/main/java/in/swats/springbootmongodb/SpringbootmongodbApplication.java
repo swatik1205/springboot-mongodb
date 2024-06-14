3c5fab3abd0c491393f49c4bdb616ddd2e110174
swatik1205 <137981584+swatik1205@users.noreply.github.com>
2024-06-14 13:38:21 -0500

    Changes made for enabling swagger

--- src/main/java/in/swats/springbootmongodb/SpringbootmongodbApplication.java
+++ src/main/java/in/swats/springbootmongodb/SpringbootmongodbApplication.java
@@ -3,9 +3,11 @@ package in.swats.springbootmongodb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableAutoConfiguration
+//@EnableSwagger2
 public class SpringbootmongodbApplication {
 
 	public static void main(String[] args) {
--- src/main/java/in/swats/springbootmongodb/service/TodoServiceImpl.java
+++ src/main/java/in/swats/springbootmongodb/service/TodoServiceImpl.java
@@ -5,8 +5,6 @@ import in.swats.springbootmongodb.model.TodoDTO;
 import in.swats.springbootmongodb.repository.TodoRepository;
 import jakarta.validation.ConstraintViolationException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
